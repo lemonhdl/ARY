0dee82160816219f0c3425754b8a6c91b171215e
lemonhdl <lemonhdl@users.noreply.github.com>
2026-06-07 12:41:56 +0800

    Create GRS001 submission mirror

--- PoC/src/start-organizer.js
+++
@@ -1,9 +0,0 @@
-import { createOrganizerServer } from './organizer-server.js';
-
-const organizerPort = Number(process.env.ORGANIZER_PORT || 4101);
-
-createOrganizerServer(organizerPort);
-
-console.log(`Organizer local data processor: http://127.0.0.1:${organizerPort}`);
-console.log('Keep this terminal open while demonstrating the available state.');
-console.log('Stop it with Ctrl+C to prove ARY does not persist Organizer data.');
